1dade459c1ce913bfd1b1b89c40b7696f4573e6a
Frenki <123064601+mikhailgutsu@users.noreply.github.com>
2024-07-11 12:09:58 +0300

    logger-v1

--- exec/main.cpp
+++ exec/main.cpp
@@ -5,13 +5,19 @@ using namespace std;
 
 int main()
 {
-    cout << endl << "Start" << endl;
+    // Specify the full path to your log file
+    string logFilePath = "AUTOSAR-SIMULATION/FS_AUTOSAR_V1/build/log/logfile.txt";
 
-    Logger logger("logfile.txt");
+    // Ensure directory for log file exists
+    string directory = logFilePath.substr(0, logFilePath.find_last_of('/'));
+    system(("mkdir -p " + directory).c_str());
+
+    // Initialize logger
+    Logger logger(logFilePath);
+
+    // Log messages
     logger.log("This is a log message.");
     logger.log("Another log message.");
-
-    cout << endl << "success" << endl;
 
     return 0;
 }
