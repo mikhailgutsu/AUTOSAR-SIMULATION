97d493104eb02021778aa3d5f17e539cfdf2d848
Frenki <123064601+mikhailgutsu@users.noreply.github.com>
2024-07-11 11:45:25 +0300

    v1

--- exec/main.cpp
+++ exec/main.cpp
@@ -5,9 +5,13 @@ using namespace std;
 
 int main()
 {
+    cout << endl << "Start" << endl;
+
     Logger logger("logfile.txt");
     logger.log("This is a log message.");
     logger.log("Another log message.");
+
+    cout << endl << "success" << endl;
 
     return 0;
 }
