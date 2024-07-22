57738007af7d192e8ca0847c6cba7dfef608512f
Frenki <123064601+mikhailgutsu@users.noreply.github.com>
2024-07-22 15:46:14 +0300

    dev-menu-digit

---
+++ exec/startMenu/incl/menu.h
@@ -0,0 +1,9 @@
+#ifndef MENU_H
+#define MENU_H
+
+void startMenu1();
+void startMenu2();
+void startMenu3();
+void startMenu4();
+
+#endif // MENU_H
--- exec/startMenu/src/menu.cpp
+++ exec/startMenu/src/menu.cpp
@@ -1,2 +1,49 @@
 #include "../incl/menu.h"
 
+void startMenu1()
+{
+    system("cls");
+
+    std::cout << " --------------------------------------------------- " << std::endl;
+    std::cout << " | > 1.   Build Option                             | " << std::endl;
+    std::cout << " |   2.                                            | " << std::endl;
+    std::cout << " |   3.                                            | " << std::endl;
+    std::cout << " |   4.                                            | " << std::endl;
+    std::cout << " --------------------------------------------------- " << std::endl;
+}
+
+void startMenu2()
+{
+    system("cls");
+
+    std::cout << " --------------------------------------------------- " << std::endl;
+    std::cout << " |   1.   Build Option                             | " << std::endl;
+    std::cout << " | > 2.                                            | " << std::endl;
+    std::cout << " |   3.                                            | " << std::endl;
+    std::cout << " |   4.                                            | " << std::endl;
+    std::cout << " --------------------------------------------------- " << std::endl;
+}
+
+void startMenu3()
+{
+    system("cls");
+
+    std::cout << " --------------------------------------------------- " << std::endl;
+    std::cout << " |   1.   Build Option                             | " << std::endl;
+    std::cout << " |   2.                                            | " << std::endl;
+    std::cout << " | > 3.                                            | " << std::endl;
+    std::cout << " |   4.                                            | " << std::endl;
+    std::cout << " --------------------------------------------------- " << std::endl;
+}
+
+void startMenu4()
+{
+    system("cls");
+
+    std::cout << " --------------------------------------------------- " << std::endl;
+    std::cout << " |   1.   Build Option                             | " << std::endl;
+    std::cout << " |   2.                                            | " << std::endl;
+    std::cout << " |   3.                                            | " << std::endl;
+    std::cout << " | > 4.                                            | " << std::endl;
+    std::cout << " --------------------------------------------------- " << std::endl;
+}
