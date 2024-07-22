4f8248ade5a6be1a230e8cc5c7368ccacdd657f6
Frenki <123064601+mikhailgutsu@users.noreply.github.com>
2024-07-22 09:31:58 +0300

    build-stub

--- _System/incl/build.h
+++ _System/incl/build.h
@@ -9,8 +9,20 @@ public:
     Build();
     ~Build();
     
+    //   Build Normal Options
     char All();
     char withoutPreprecess();
+    char Preprocess();
+    char preprocessWithoutCusReprog();
+    char Compile();
+    char compileLink();
+    char reportGenerator();
+    char RUNIN_GUI_Fast();
+    char RUNIN_GUI_Slow();
+    char Tessy();
+    char PCLINT();
+    char PCLINT_ALL();
+    char tresosCnf();
 };
 
 #endif // BUILD_H
--- _System/mainBuild.cpp
+++ _System/mainBuild.cpp
@@ -15,13 +15,79 @@ extern char BuildChoise(char tmp)
             cout << "0x01" << endl;
 
             b1.All();
-            // To be continue..
+                // To be continue..
             break;
         case 0x02:
             cout << "0x02" << endl;
 
             b1.withoutPreprecess();
-            // To be continue..
+                // To be continue..
+            break;
+        case 0x03:
+            cout << "0x03" << endl;
+
+            b1.Preprocess();
+                // To be continue..
+            break;
+        case 0x04:
+            cout << "0x04" << endl;
+
+            b1.preprocessWithoutCusReprog();
+                // To be continue..
+            break;
+        case 0x05:
+            cout << "0x05" << endl;
+
+            b1.Compile();
+                // To be continue..
+            break;
+        case 0x06:
+            cout << "0x06" << endl;
+
+            b1.compileLink();
+                // To be continue..
+            break;
+        case 0x07:
+            cout << "0x07" << endl;
+
+            b1.reportGenerator();
+                // To be continue..
+            break;
+        case 0x08:
+            cout << "0x08" << endl;
+
+            b1.RUNIN_GUI_Fast();
+                 // To be continue..
+            break;
+        case 0x09:
+            cout << "0x09" << endl;
+
+            b1.RUNIN_GUI_Slow();
+                 // To be continue..
+            break;
+        case 0x0A:
+            cout << "0x0A" << endl;
+
+            b1.Tessy();
+                // To be continue..
+            break;
+        case 0x0B:
+            cout << "0x0B" << endl;
+
+            b1.PCLINT();
+                // To be continue..
+            break;
+        case 0x0C:
+            cout << "0x0C" << endl;
+
+            b1.PCLINT_ALL();
+                // To be continue..
+            break;
+        case 0x0D:
+            cout << "0x0D" << endl;
+
+            b1.tresosCnf();
+                // To be continue..
             break;
         default:
             cout << "default" << endl;
