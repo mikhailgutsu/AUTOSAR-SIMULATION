4207fb0469c1e6015d73edb8758aec37818d49b7
Frenki <123064601+mikhailgutsu@users.noreply.github.com>
2024-07-24 15:37:17 +0300

    Update terminal.cpp

--- exec/startMenu/src/terminal.cpp
+++ exec/startMenu/src/terminal.cpp
@@ -34,24 +34,25 @@ void terminalWelcome()
 void terminalHelp()
 {
     std::cout << std::endl << std::endl << "----- Command Panel -------------------------------" << std::endl;
-    std::cout << "  1.  --main" << std::endl;
-    std::cout << "  2.  bbb" << std::endl;
-    std::cout << "  3.  ccc" << std::endl;
-    std::cout << "  4.  ddd" << std::endl;
-    std::cout << "  5.  eee" << std::endl;
-    std::cout << "  6.  fff" << std::endl << std::endl;
+    std::cout << "  1.  --main      " << std::endl;
+    std::cout << "  2.  --info" << std::endl;
+    std::cout << "  3.  --check DCM" << std::endl;
+    std::cout << "  4.  --check COM" << std::endl;
+    std::cout << "  5.  --check MEM" << std::endl;
+    std::cout << "  6.  --back" << std::endl << std::endl;
 
     system("pause"); 
 }
 
 int terminal()
 {
+    bool status = TRUE;
     terminalLoading();
 
     std::pmr::string commandINPUT;
     int commandINPUT_ID = 0;
 
-    while(true)
+    while(status)
     {
         terminalWelcome();
 
@@ -60,15 +61,31 @@ int terminal()
         {
             commandINPUT_ID = 1;
         }
-        if (commandINPUT == "--main")
+            if (commandINPUT == "--main")
+            {
+                commandINPUT_ID = 2;
+            }
+        if (commandINPUT == "--check DCM")
         {
-            commandINPUT_ID = 2;
+            commandINPUT_ID = 3;
         }
+            if (commandINPUT == "--check COM")
+            {
+                commandINPUT_ID = 4;
+            }
+        if (commandINPUT == "--check MEM")
+        {
+            commandINPUT_ID = 5;
+        }
+            if (commandINPUT == "--back")
+            {
+                commandINPUT_ID = 6;
+            }
 
         switch(commandINPUT_ID)
         {
             case 0:
-                    std::cout << std::endl << commandINPUT << " - Isn't internal/external command. Please use '/help' for information." << std::endl << std::endl;
+                    std::cout << std::endl << commandINPUT << " - Isn't internal/external command. Please use '/help' for information." << std::endl;
 
                     system("pause");
                 break;
@@ -76,13 +93,19 @@ int terminal()
                     terminalHelp();
                 break;
             case 2:
+
+                break;
             case 3:
+
+                break;
             case 4:
+             
+                break;
             case 5:
+            
+                break;
             case 6:
-            case 7:
-            case 8:
-            case 9:
+                status = FALSE;
                 break;
         }
         commandINPUT_ID = 0;
