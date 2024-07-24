7701f9605c92f3e0a53f9fb6d688cdf05789a9db
Frenki <123064601+mikhailgutsu@users.noreply.github.com>
2024-07-24 17:54:53 +0300

    +DCM

--- work/bsw/diag/uds/serv_22_cnf/i/serv22_cnf_man.cpp
+++ work/bsw/diag/uds/serv_22_cnf/i/serv22_cnf_man.cpp
@@ -1,2 +1,69 @@
 #include "serv22_cnf_man.h"
 
+//<--------------------------------------------------------------------------------------------------------------- DCM CLASS
+Dcm::Dcm(char x1, char x2)
+{
+    firstBYTE = x1;
+    secondBYTE = x2;
+}
+
+Dcm::~Dcm(){}
+
+char Dcm::Push(char temp)
+{
+    positiveResponse = 22 + 40;
+    additionalBYTE = temp;
+
+    std::cout << positiveResponse  << " " << firstBYTE << " " << secondBYTE << " " << additionalBYTE << std::endl;
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//<---------------------------------------------------------------------------------------------------------------  STUBS
+char VIN_GETTER(char temp)
+{
+    temp = VIN;
+        return temp;
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+char DCM_ReadDataByIdentifier_VIN_F190(char x1, char x2, int UTEST = 0)
+{
+    /*
+        SIGNAL INFORMATION:
+            MULT = 1;
+            DIV = 1;
+            ADD = 1;
+    
+        INPUT:
+            unsigned char: 0x22 - SID
+            unsigned char: 0xF1 - 1st BYTE
+            unsigned char: 0x90 - 2nd BYTE
+        
+        DisplayValue:
+            MUL * CoddedValue / DIV + ADD
+    */
+
+    char RetVal = DCM_IN_HOLD;
+    char temp;
+    VIN_GETTER(temp);
+
+    Dcm d_temp(x1, x2);
+    
+    //<--------------------------------------------------------------------------------------------------------------- UTEST SEGMENT
+    if ((temp == VIN) && (UTEST == 1))
+    {
+        RetVal = E_OK;
+        d_temp.Push(temp);
+        return RetVal;
+    }
+        else 
+        {   
+            d_temp.Push(temp);
+            RetVal = E_NOT_OK;
+            return RetVal;
+        }
+
+    return RetVal;
+}
--- work/bsw/diag/uds/serv_22_cnf/i/serv22_cnf_man.h
+++ work/bsw/diag/uds/serv_22_cnf/i/serv22_cnf_man.h
@@ -1,6 +1,27 @@
 #ifndef SERV22_H
 #define SERV22_H
 
+#include <iostream>
 
+#define VIN 0xFF
+#define E_OK 0x01
+#define E_NOT_OK 0x00
+#define DCM_IN_HOLD 0x1A
+
+class Dcm {
+private:
+    char positiveResponse;
+    char firstBYTE, secondBYTE;
+    char additionalBYTE;
+
+public:
+    Dcm(char x1, char x2);
+    ~Dcm();
+    char Push(char temp);
+};
+
+char VIN_GETTER(char temp);
+
+char DCM_ReadDataByIdentifier_VIN_F190();
 
 #endif //SERV22_H
