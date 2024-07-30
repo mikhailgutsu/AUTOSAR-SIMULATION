981f96f7b41fa0f2d67453ff5dcf1994f76cfa53
Frenki <123064601+mikhailgutsu@users.noreply.github.com>
2024-07-30 09:37:24 +0300

    Update build.cpp

--- _System/src/build.cpp
+++ _System/src/build.cpp
@@ -1,6 +1,11 @@
 #include "../incl/build.h"
 #include <windows.h>
 
+//DEFINE USER
+#define E_OK                 0x01
+#define E_NOT_OK             0x00
+//////////////////////////////
+
 //STACK INCLUDE
 #include "../../exec/logger/src/logger.cpp"
 
@@ -15,6 +20,82 @@
 #include "../../work/bsw/diag/obd/obd.cpp"
 ///////////////
 
+char Init_Checker()
+{
+    Logger message;
+    char RetVal = E_NOT_OK;
+    int counter = 0;
+
+    if (app_Init() == E_OK)
+    {   
+        SetColor(10);
+        message.log("Initialization APP - passed");
+        counter++;
+    }
+    Sleep(200);
+    if (dds_Init() == E_OK)
+    {
+        SetColor(10);
+        message.log("Initialization DDS - passed");
+        counter++;
+    }
+    Sleep(200);
+    if (mcal_Init() == E_OK)
+    {
+        SetColor(10);
+        message.log("Initialization MCAL - passed");
+        counter++;
+    }
+    Sleep(200);
+    if (rte_Init() == E_OK)
+    {
+        SetColor(10);
+        message.log("Initialization RTE - passed");
+        counter++;
+    }
+    Sleep(200);
+    if (cdd_Init() == E_OK)
+    {
+        SetColor(10);
+        message.log("Initialization CDD - passed");
+        counter++;
+    }
+    Sleep(200);
+    if (com_Init() == E_OK)
+    {
+        SetColor(10);
+        message.log("Initialization COM - passed");
+        counter++;
+    }
+    Sleep(200);
+    if (mem_Init() == E_OK)
+    {
+        SetColor(10);
+        message.log("Initialization MEM - passed");
+        counter++;
+    }
+    Sleep(200);
+    if (sec_Init() == E_OK)
+    {
+        SetColor(10);
+        message.log("Initialization SEC - passed");
+        counter++;
+    }
+    Sleep(200);
+    if (obd_Init() == E_OK)
+    {
+        SetColor(10);
+        message.log("Initialization OBD - passed");
+        counter++;
+    }
+    Sleep(2000);
+    if(counter == 9)
+    {
+        RetVal = E_OK;
+    }
+    return RetVal;
+}
+
 Build::Build(){}
 Build::~Build(){}
 
@@ -22,149 +103,194 @@ char Build::All()
 {
     Logger message;
     system("cls");
-    Sleep(300);
+    Sleep(500);
 
-    if (app_Init() == E_OK)
-    {   
-        SetColor(10);
-        message.log("Initialization APP - passed");
-    }
-    Sleep(200);
-    if (dds_Init() == E_OK)
+    if (Init_Checker() == E_OK)
     {
-        SetColor(10);
-        message.log("Initialization DDS - passed");
+        message.log("Stack Initialization was done with success.");
+        Sleep(500);
     }
-    Sleep(200);
-    if (mcal_Init() == E_OK)
-    {
-        SetColor(10);
-        message.log("Initialization MCAL - passed");
-    }
-    Sleep(200);
-    if (rte_Init() == E_OK)
-    {
-        SetColor(10);
-        message.log("Initialization RTE - passed");
-    }
-    Sleep(200);
-    if (cdd_Init() == E_OK)
-    {
-        SetColor(10);
-        message.log("Initialization CDD - passed");
-    }
-    Sleep(200);
-    if (com_Init() == E_OK)
-    {
-        SetColor(10);
-        message.log("Initialization COM - passed");
-    }
-    Sleep(200);
-    if (mem_Init() == E_OK)
-    {
-        SetColor(10);
-        message.log("Initialization MEM - passed");
-    }
-    Sleep(200);
-    if (sec_Init() == E_OK)
-    {
-        SetColor(10);
-        message.log("Initialization SEC - passed");
-    }
-    Sleep(200);
-    if (obd_Init() == E_OK)
-    {
-        SetColor(10);
-        message.log("Initialization OBD - passed");
-    }
-    Sleep(2000);
-
     SetColor(15);
-
     return 0x01;
 }
 
 char Build::withoutPreprecess()
 {
-    std::cout << "Build withoutPreprecess success" << std::endl;
-    Sleep(100);
+    Logger message;
+    system("cls");
+    Sleep(500);
+
+    if (Init_Checker() == E_OK)
+    {
+        message.log("Stack Initialization was done with success.");
+        Sleep(500);
+    }
+    SetColor(15);
     return 0x02;
 }
 
 char Build::Preprocess()
 {
-    std::cout << "Build Preprocess success" << std::endl;
-    Sleep(100);
+    Logger message;
+    system("cls");
+    Sleep(500);
+
+    if (Init_Checker() == E_OK)
+    {
+        message.log("Stack Initialization was done with success.");
+        Sleep(500);
+    }
+    SetColor(15);
     return 0x03;
 }
 
 char Build::preprocessWithoutCusReprog()
 {
-    std::cout << "Build preprocessWithoutCusReprog success" << std::endl;
-    Sleep(100);
+    Logger message;
+    system("cls");
+    Sleep(500);
+
+    if (Init_Checker() == E_OK)
+    {
+        message.log("Stack Initialization was done with success.");
+        Sleep(500);
+    }
+    SetColor(15);
     return 0x04;
 }
 
 char Build::Compile()
 {
-    std::cout << "Build Compile success" << std::endl;
-    Sleep(100);
+    Logger message;
+    system("cls");
+    Sleep(500);
+
+    if (Init_Checker() == E_OK)
+    {
+        message.log("Stack Initialization was done with success.");
+        Sleep(500);
+    }
+    SetColor(15);
     return 0x05;
 }
 
 char Build::compileLink()
 {
-    std::cout << "Build compileLink success" << std::endl;
-    Sleep(100);
+    Logger message;
+    system("cls");
+    Sleep(500);
+
+    if (Init_Checker() == E_OK)
+    {
+        message.log("Stack Initialization was done with success.");
+        Sleep(500);
+    }
+    SetColor(15);
     return 0x06;
 }
 
 char Build::reportGenerator()
 {
-    std::cout << "Build reportGenerator success" << std::endl;
-    Sleep(100);
+     Logger message;
+    system("cls");
+    Sleep(500);
+
+    if (Init_Checker() == E_OK)
+    {
+        message.log("Stack Initialization was done with success.");
+        Sleep(500);
+    }
+    SetColor(15);
     return 0x07;
 }
 
 char Build::RUNIN_GUI_Fast()
 {
-    std::cout << "Build RUNIN_GUI_Fast success" << std::endl;
-    Sleep(100);
+    Logger message;
+    system("cls");
+    Sleep(500);
+
+    if (Init_Checker() == E_OK)
+    {
+        message.log("Stack Initialization was done with success.");
+        Sleep(500);
+    }
+    SetColor(15);
     return 0x08;
 }
 
 char Build::RUNIN_GUI_Slow()
 {
-    std::cout << "Build RUNIN_GUI_Slow success" << std::endl;
-    Sleep(100);
+    Logger message;
+    system("cls");
+    Sleep(500);
+
+    if (Init_Checker() == E_OK)
+    {
+        message.log("Stack Initialization was done with success.");
+        Sleep(500);
+    }
+    SetColor(15);
     return 0x09;
 }
 
 char Build::Tessy()
 {
-    std::cout << "Build Tessy success" << std::endl;
-    Sleep(100);
+    Logger message;
+    system("cls");
+    Sleep(500);
+
+    if (Init_Checker() == E_OK)
+    {
+        message.log("Stack Initialization was done with success.");
+        Sleep(500);
+    }
+    SetColor(15);
     return 0x0A;
 }
 
 char Build::PCLINT()
 {
-    std::cout << "Build PCLINT success" << std::endl;
-    Sleep(100);
+    Logger message;
+    system("cls");
+    Sleep(500);
+
+    if (Init_Checker() == E_OK)
+    {
+        message.log("Stack Initialization was done with success.");
+        Sleep(500);
+    }
+    SetColor(15);
     return 0x0B;
 }
 
 char Build::PCLINT_ALL()
 {
-    std::cout << "Build PCLINT_ALL success" << std::endl;
-    Sleep(100);
+    Logger message;
+    system("cls");
+    Sleep(500);
+
+    if (Init_Checker() == E_OK)
+    {
+        message.log("Stack Initialization was done with success.");
+        Sleep(500);
+    }
+    SetColor(15);
     return 0x0C;
 }
 
 char Build::tresosCnf()
 {
-    std::cout << "Build tresosCnf success" << std::endl;
-    Sleep(100);
+    Logger message;
+    system("cls");
+    Sleep(500);
+
+    if (Init_Checker() == E_OK)
+    {
+        message.log("Stack Initialization was done with success.");
+        Sleep(500);
+    }
+    SetColor(15);
     return 0x0D;
 }
 
